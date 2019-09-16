d5a04af8d18e378cbc75a4edbe20e3356a846c60
karlo <deguzmankarlo@gmail.com>
2019-09-16 17:29:11 +0100

    Fizzbuzz 3, 5 and 15 working

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -2,4 +2,24 @@ describe("fizzBuzz", function() {
   it("Should return fizz if 3 is passed in", function() {
     expect(fizzBuzz(3)).toEqual("fizz")
   });
+
+  it("Should return buzz if 5 is passed in", function() {
+    expect(fizzBuzz(5)).toEqual("buzz")
+  });
+
+  it("Should return fizzbuzz if 15 is passed in", function() {
+    expect(fizzBuzz(15)).toEqual("fizzbuzz")
+  });
+
+  it("Should return fizz if 6 is passed in", function() {
+    expect(fizzBuzz(6)).toEqual("fizz")
+  });
+
+  it("Should return buzz if 10 is passed in", function() {
+    expect(fizzBuzz(10)).toEqual("buzz")
+  });
+
+  it("Should return fizzbuzz if 30 is passed in", function() {
+    expect(fizzBuzz(30)).toEqual("fizzbuzz")
+  });
 });
